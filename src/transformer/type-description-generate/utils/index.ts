d11511c39bf3968055291c2aeaec3a4b501d2622
Valerii Pozdniakov <vpposd@gmail.com>
2025-09-11 19:45:25 +0200

    extract syntax kind and handle array type in type description utility functions

--- src/transformer/type-description-generate/utils/index.ts
+++ src/transformer/type-description-generate/utils/index.ts
@@ -3,6 +3,58 @@ import { first } from 'lodash/fp';
 
 import { PropertyType } from '../../../types';
 
+const extractSyntaxKind = (
+  propertySignature:
+    | ts.PropertySignature
+    | ts.PropertyDeclaration
+    | ts.TypeNode
+    | undefined,
+): ts.SyntaxKind | undefined => {
+  if (!propertySignature) {
+    return undefined;
+  }
+
+  let kind: ts.SyntaxKind | undefined = propertySignature.kind;
+
+  if (ts.isLiteralTypeNode(propertySignature)) {
+    kind = propertySignature.literal.kind;
+  }
+
+  if (
+    ts.isPropertyDeclaration(propertySignature) ||
+    ts.isPropertySignature(propertySignature)
+  ) {
+    kind = propertySignature.type && propertySignature.type.kind;
+  }
+
+  return kind;
+};
+
+const handleArrayType = (
+  propertySignature:
+    | ts.PropertySignature
+    | ts.PropertyDeclaration
+    | ts.TypeNode
+    | undefined,
+): PropertyType => {
+  if (!propertySignature) {
+    return PropertyType.Unknown;
+  }
+
+  if (
+    ts.isPropertyDeclaration(propertySignature) ||
+    ts.isPropertySignature(propertySignature)
+  ) {
+    return getPropertyNameBySyntaxKind(
+      (propertySignature.type as ts.ArrayTypeNode).elementType,
+    );
+  }
+
+  return getPropertyNameBySyntaxKind(
+    (propertySignature as ts.ArrayTypeNode).elementType,
+  );
+};
+
 export const getTypeNameByWrapperFunction = (name: string) => {
   switch (name) {
     case 'Number':
@@ -32,16 +84,9 @@ export const getPropertyNameBySyntaxKind = (
   if (!propertySignature) {
     return PropertyType.Unknown;
   }
-  let kind: ts.SyntaxKind | undefined = propertySignature.kind;
-  if (ts.isLiteralTypeNode(propertySignature)) {
-    kind = propertySignature.literal.kind;
-  }
-  if (
-    ts.isPropertyDeclaration(propertySignature) ||
-    ts.isPropertySignature(propertySignature)
-  ) {
-    kind = propertySignature.type && propertySignature.type.kind;
-  }
+
+  const kind = extractSyntaxKind(propertySignature);
+
   switch (kind) {
     case ts.SyntaxKind.StringKeyword:
       return PropertyType.String;
@@ -58,14 +103,7 @@ export const getPropertyNameBySyntaxKind = (
     case ts.SyntaxKind.UndefinedKeyword:
       return PropertyType.Undefined;
     case ts.SyntaxKind.ArrayType:
-      return ts.isPropertyDeclaration(propertySignature) ||
-        ts.isPropertySignature(propertySignature)
-        ? getPropertyNameBySyntaxKind(
-            (propertySignature.type as ts.ArrayTypeNode).elementType,
-          )
-        : getPropertyNameBySyntaxKind(
-            (propertySignature as ts.ArrayTypeNode).elementType,
-          );
+      return handleArrayType(propertySignature);
     default:
       return PropertyType.Unknown;
   }
